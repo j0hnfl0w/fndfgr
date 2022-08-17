ac49aff39295ef9106a13724ba72ccc30f5b5131
j0hnfl0w <ij69@pm.me>
2022-08-17 14:17:06 +0300

    fixed texts in voids mocks

--- src/groups/void-video/PageIndex.vue
+++ src/groups/void-video/PageIndex.vue
@@ -2,12 +2,8 @@
 .row.full-width.items-center.content-center.justify-center.window-height.q-px-md
   div(:style="{position: 'relative', maxWidth: '900px'}").row.full-width
     div(:style="{position: 'relative', paddingBottom: '56%'}").row.full-width
-      div(:style="{position: 'absolute', zIndex: 1, borderRadius: '8px', fontSize: '18px'}").column.fit.q-pa-md.bg-black.text-white
+      div(:style="{position: 'absolute', zIndex: 1, borderRadius: '8px', fontSize: '16px'}").column.fit.q-pa-md.bg-black.text-white
         p imagin creating fgrs from youtube videos?
         p okay, capture cool moments from videos and share them with friends
         p mint them as nfts and sell, sell your point of view on this cool moments
-        p .
-        p .
-        p .
-        p @fndfgr
 </template>
